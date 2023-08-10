bbc30b5533188a97c234ca99f2491284da5fe11e
PamKhumalo <theepamelakhumalo@gmail.com>
2023-08-10 06:18:04 -0700

    Hello, world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#!/bin/bash
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - Prints with puts
  *
  * Return: Always 0 (Success)
  */
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
-#!/bin/bash
 #include<stdio.h>
 /**
-* main - Entry point
+* main - Prints the size of various types
 *
 * Return: Always 0 (Success)
 */
